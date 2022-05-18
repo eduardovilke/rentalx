a702adaa71f57fbbed847d785073cd5b87d8fa41
Eduardo Vilke <eduardo.svilke@gmail.com>
2022-05-18 11:40:23 -0300

    refactory specifications

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,7 @@
 import { parse } from "csv-parse";
 import fs from "fs";
+import { inject, injectable } from "tsyringe";
+
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IImportCategory {
@@ -7,23 +9,27 @@ interface IImportCategory {
   description: string;
 }
 
+@injectable()
 class ImportCategoryUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository){}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+  ) {}
 
   loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
     return new Promise((resolve, reject) => {
       const stream = fs.createReadStream(file.path);
-      const categories : IImportCategory[] = []
-      
+      const categories: IImportCategory[] = [];
+
       const parseFile = parse();
-      stream.pipe(parseFile)
-    
+      stream.pipe(parseFile);
+
       parseFile
         .on("data", async (line) => {
           const [name, description] = line;
           categories.push({
-            name, 
-            description
+            name,
+            description,
           });
         })
         .on("end", () => {
@@ -32,26 +38,26 @@ class ImportCategoryUseCase {
         })
         .on("error", (err) => {
           reject(err);
-        })
+        });
     });
   }
-  
+
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
 
     categories.map(async (category) => {
       const { name, description } = category;
 
-      const existCategory = this.categoriesRepository.findByName(name);
+      const existCategory = await this.categoriesRepository.findByName(name);
 
-      if (!existCategory){
-        this.categoriesRepository.create({
+      if (!existCategory) {
+        await this.categoriesRepository.create({
           name,
-          description
-        })
+          description,
+        });
       }
-    })
+    });
   }
 }
 
-export { ImportCategoryUseCase }
+export { ImportCategoryUseCase };
--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
-  constructor(private ListCategoriesUseCase: ListCategoriesUseCase) {}
   async handle(request: Request, response: Response): Promise<Response> {
-    const all = await this.ListCategoriesUseCase.execute();
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
+
+    const all = await listCategoriesUseCase.execute();
 
     return response.json(all);
   }
