b30dc75ef8efd4dbf3eb0e45458438242e375877
Eduardo Vilke <eduardo.svilke@gmail.com>
2022-06-02 11:10:57 -0300

    feat: set middlewares specifications routes

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpecificationsController } from "@modules/cars/useCases/listSpecification/ListSpecificationsController";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
@@ -10,9 +11,16 @@ const specificationsRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationsController = new ListSpecificationsController();
 
-specificationsRoutes.post("/", createSpecificationController.handle);
-
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.get("/", listSpecificationsController.handle);
+specificationsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationController.handle
+);
+specificationsRoutes.get(
+  "/",
+  ensureAuthenticated,
+  listSpecificationsController.handle
+);
 
 export { specificationsRoutes };
