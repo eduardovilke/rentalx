d9d5703ed152c2cd34fd68c326b7d50702b39885
Eduardo Vilke <eduardo.svilke@gmail.com>
2022-06-09 10:58:10 -0300

    feat: create cars specifications

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -14,13 +14,18 @@ class SpecificationsRepository implements ISpecificationsRepository {
     this.repository = getRepository(Specification);
   }
 
-  async create({ description, name }: ICreateSpecificationDTO): Promise<void> {
+  async create({
+    description,
+    name,
+  }: ICreateSpecificationDTO): Promise<Specification> {
     const specification = this.repository.create({
       name,
       description,
     });
 
     await this.repository.save(specification);
+
+    return specification;
   }
 
   async list(): Promise<Specification[]> {
@@ -30,8 +35,14 @@ class SpecificationsRepository implements ISpecificationsRepository {
   }
 
   async findByName(name: string): Promise<Specification> {
-    const specification = this.repository.findOneBy({ name });
+    const specification = await this.repository.findOneBy({ name });
     return specification;
+  }
+
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const specfications = await this.repository.findByIds(ids);
+
+    return specfications;
   }
 }
 
